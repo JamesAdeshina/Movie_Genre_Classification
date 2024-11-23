7edadbea973ec6cbe49e097f67218ce32f7623b7
James <adeshina.james@bowen.edu.ng>
2024-11-23 23:29:58 +0000

    Fixing train_model function bug...

--- src/train_model.py
+++ src/train_model.py
@@ -2,13 +2,13 @@ from sklearn.multioutput import MultiOutputClassifier
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
-def train_classifier(X_train, y_train):
-    """
-    Train a multi-label classifier using Random Forest.
-    """
-    model = MultiOutputClassifier(RandomForestClassifier(n_estimators=100, random_state=42))
-    model.fit(X_train, y_train)
-    return model
+# def train_classifier(X_train, y_train):
+#     """
+#     Train a multi-label classifier using Random Forest.
+#     """
+#     model = MultiOutputClassifier(RandomForestClassifier(n_estimators=100, random_state=42))
+#     model.fit(X_train, y_train)
+#     return model
 
 def save_model(model, vectorizer, class_labels, model_save_path):
     """
